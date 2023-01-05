da95e99d3b552995d339974fcdd746aa8b4fae87
MaisterGarcia <jorge_08emmanuel@hotmail.com>
2023-01-05 14:39:37 -0600

    ajustes 2

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -12,21 +12,22 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const autenticarUsuario = async () => {
-      const token = localStorage.getItem("token");
-
-      if (!token) {
-        setCargando(false);
-        return;
-      }
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
       try {
-        const { data } = await clienteAxios("/usuarios/perfil", config);
-        setAuth(data);
+        setCargando(true);
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          const config = {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          };
+
+          const { data } = await clienteAxios("/usuarios/perfil", config);
+          setAuth(data);
+        }
+
         // navigate("/proyectos");
       } catch (error) {
         console.log(error.response);
@@ -35,9 +36,8 @@ const AuthProvider = ({ children }) => {
         setCargando(false);
       }
     };
-
     return () => {
-      autenticarUsuario();
+      return autenticarUsuario();
     };
   }, []);
   const cerrarSesionAuth = () => {
